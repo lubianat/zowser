f364f3f702e853ca73cd34498935d24a65702eb8
Tiago Lubiana <tiago.lubiana.alves@usp.br>
2025-10-13 14:06:22 -0300

    Fix viteconfig for /zowser

--- vite.config.js
+++ vite.config.js
@@ -37,7 +37,7 @@ let config = {
 };
 // this will be undefined when deployed from netlify, but is used by gh-pages
 if (process.env.GITHUB_REPOSITORY_OWNER) {
-  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/ome2024-ngff-challenge/`;
+  config.base = `https://${process.env.GITHUB_REPOSITORY_OWNER}.github.io/zowser/`;
 }
 
 // https://vitejs.dev/config/
